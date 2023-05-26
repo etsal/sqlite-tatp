11f3929e8cb76f00fdf6eca21a90df2e6bbeb748
Aimilios Tsalapatis <atsalapa@uwaterloo.ca>
2023-05-26 09:53:00 -0400

    removed SSB benchmark and DDB workload

--- scripts/benchmarks/tatp.sh
+++ scripts/benchmarks/tatp.sh
@@ -17,21 +17,4 @@ for sf in 10000 100000 1000000; do
       done
     done
   done
-
-  rm tatp.sqlite
-
-  printf "Loading data into DuckDB...\n"
-  ./tatp_duckdb --load --records=$sf
-
-  printf "Evaluating DuckDB...\n"
-  for memory_limit in "100MB" "200MB" "500MB" "1GB" "2GB" "5GB"; do
-    command="./tatp_duckdb --run --records=$sf --memory_limit=$memory_limit"
-    printf "%s\n" "$command"
-    for trial in {1..3}; do
-      printf "%s," "$trial"
-      eval "$command"
-    done
-  done
-
-  rm tatp.duckdb
 done
